143dd0a8d75daa343a578640dce5e1faaeee340a
Sigfrid Dusci <sig@inforlife.ch>
2018-06-14 14:35:22 +0200

    migrate JS to Webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,10 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+
+import Rails from 'rails-ujs'
+Rails.start()
+
+import Turbolinks from 'turbolinks'
+Turbolinks.start()
