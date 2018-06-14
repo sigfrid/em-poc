396f3c164724859802f8c48ea6c2c4a6d8394636
Sigfrid Dusci <sig@inforlife.ch>
2018-06-14 14:09:08 +0200

    add password toggle

--- app/javascript/packs/controllers/samples_controller.js
+++ app/javascript/packs/controllers/samples_controller.js
@@ -1,7 +1,37 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  log() {
-    console.log("HelloWorld")
+  initialize() {
+    this.textFieldElement.placeholder = this.placeholderWhenUnmasked
   }
+
+  toggle() {
+    this.textFieldElement.setAttribute('type', this.otherValue(this.textFieldCurrentType))
+    if(this.textFieldCurrentType == 'text') {
+      this.textFieldElement.placeholder = this.placeholderWhenUnmasked
+    } else {
+      this.textFieldElement.placeholder = this.placeholderWhenMasked
+    }
+  }
+
+  get textFieldElement() {
+    return this.targets.find("textfield")
+  }
+
+  get textFieldCurrentType() {
+    return this.textFieldElement.getAttribute('type')
+  }
+
+  get placeholderWhenUnmasked() {
+    return this.data.get("placeholder")
+  }
+
+  get placeholderWhenMasked() {
+    return '●●●●●●●●●●'
+  }
+
+  otherValue(currentType) {
+    return (currentType == 'text' ? 'password' : 'text')
+  }
+
 }
