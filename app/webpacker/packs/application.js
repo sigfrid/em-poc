bb98885368dac11504d4de4a74cbdc3de929e200
Sigfrid Dusci <sig@inforlife.ch>
2018-06-14 14:52:33 +0200

    migrate CSS to Webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -4,7 +4,7 @@ import { Application }            from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
+const context = require.context("../src/javascript/controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
 
