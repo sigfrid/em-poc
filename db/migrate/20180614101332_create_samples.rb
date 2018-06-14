315fb086422f26ca4b37d4f56252ab5be9bdd1ea
Sigfrid Dusci <sig@inforlife.ch>
2018-06-14 12:17:33 +0200

    add Sample model

---
+++ db/migrate/20180614101332_create_samples.rb
@@ -0,0 +1,10 @@
+class CreateSamples < ActiveRecord::Migration[5.2]
+  def change
+    create_table :samples do |t|
+      t.string :name
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
